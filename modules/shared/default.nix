0fd5d5c200ce93be5593a2e005b934bd28ce7521
bfeitknecht <bfeitknecht@ethz.ch>
2026-01-15 16:57:03 +0100

    grrrr

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -16,6 +16,6 @@
       map (n: import (path + ("/" + n)))
           (filter (n: match ".*\\.nix" n != null ||
                       pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)))
+                  (attrNames (readDir path)));
   };
 }
