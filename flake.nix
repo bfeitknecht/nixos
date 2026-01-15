84a1d784e2b4d9264f167c5fee3557c6b8725250
bfeitknecht <bfeitknecht@ethz.ch>
2026-01-15 17:33:03 +0100

    errr

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/lix-module/archive/main.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
